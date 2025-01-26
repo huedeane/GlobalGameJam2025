1138de96e92327632b3b35e4a211011c3aa60aa5
Jack Valladares <52551151+JackValladares@users.noreply.github.com>
2025-01-26 16:05:26 -0500

    Changes

--- Assets/PlayerShootBehavior.cs
+++ Assets/PlayerShootBehavior.cs
@@ -3,48 +3,94 @@ using UnityEngine;
 
 public class PlayerShootBehavior : MonoBehaviour
 {
-    
     public GameObject DefaultBubblePrefab;
 
-    //On Left Mouse Button Click, shoot a bubble in the direction of the mouse
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && PlayerStats.Instance.GetCurrentItem() == PlayerStats.ItemType.BubbleGun)
+        PlayerStats.ItemType currentItem = PlayerStats.Instance.GetCurrentItem();
+
+        if (Input.GetMouseButtonDown(0)) // Left Click
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 playerPosition = transform.position;
-
-            Vector2 direction = (mousePosition - playerPosition).normalized;
-
-            GameObject bubble = Instantiate(DefaultBubblePrefab, playerPosition, Quaternion.identity);
-            bubble.GetComponent<Projectile>().SetTargetPosition(playerPosition + direction * 1000);
-            PlayerStats.Instance.CurrentOxygen -= 10;
+            HandleLeftClick(currentItem);
         }
-        else if (Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonDown(1)) // Right Click
         {
-            PlayerStats.ItemType currentItem = PlayerStats.Instance.GetCurrentItem();
-            int ItemValue = PlayerStats.Instance.GetItemValue(currentItem);
-            if (currentItem != PlayerStats.ItemType.BubbleGun && ItemValue > 0)
-            {
-                
-                PlayerStats.Instance.SetCurrentItem(PlayerStats.ItemType.None);
-                PlayerStats.Instance.CurrentMoney += ItemValue;
-                
-            }
-            else if (currentItem == PlayerStats.ItemType.OxygenTank)
-            {
-                PlayerStats.Instance.SetCurrentItem(PlayerStats.ItemType.None);
-                PlayerStats.Instance.CurrentOxygen = Math.Min(PlayerStats.Instance.CurrentOxygen + PlayerStats.Instance.MaxOxygen/3, PlayerStats.Instance.MaxOxygen);
-            }
+            HandleRightClick(currentItem);
         }
     }
-    
-    //OnDrawGizmos, draw a line from the player to the mouse position using the same implementation as above
+
+    private void HandleLeftClick(PlayerStats.ItemType currentItem)
+    {
+        switch (currentItem)
+        {
+            case PlayerStats.ItemType.BubbleGun:
+                ShootBubble();
+                break;
+
+            case PlayerStats.ItemType.OxygenTank:
+                UseOxygenTank();
+                break;
+
+            default:
+                SellItem(currentItem);
+                break;
+        }
+    }
+
+    private void HandleRightClick(PlayerStats.ItemType currentItem)
+    {
+        switch (currentItem)
+        {
+            case PlayerStats.ItemType.FlashLight:
+                ToggleFlashlight();
+                break;
+
+            default:
+                SellItem(currentItem);
+                break;
+        }
+    }
+
+    private void ShootBubble()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerPosition = transform.position;
+        Vector2 direction = (mousePosition - playerPosition).normalized;
+
+        GameObject bubble = Instantiate(DefaultBubblePrefab, playerPosition, Quaternion.identity);
+        bubble.GetComponent<Projectile>().SetTargetPosition(playerPosition + direction * 1000);
+        PlayerStats.Instance.CurrentOxygen -= 10;
+    }
+
+    private void UseOxygenTank()
+    {
+        PlayerStats.Instance.SetCurrentItem(PlayerStats.ItemType.None);
+        PlayerStats.Instance.CurrentOxygen = Math.Min(
+            PlayerStats.Instance.CurrentOxygen + PlayerStats.Instance.MaxOxygen / 3,
+            PlayerStats.Instance.MaxOxygen
+        );
+    }
+
+    private void SellItem(PlayerStats.ItemType currentItem)
+    {
+        int itemValue = PlayerStats.Instance.GetItemValue(currentItem);
+        if (itemValue > 0)
+        {
+            PlayerStats.Instance.SetCurrentItem(PlayerStats.ItemType.None);
+            PlayerStats.Instance.CurrentMoney += itemValue;
+        }
+    }
+
+    private void ToggleFlashlight()
+    {
+        // Placeholder logic for toggling the flashlight
+        Debug.Log("Flashlight toggled");
+    }
+
     private void OnDrawGizmos()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 playerPosition = transform.position;
-
         Vector2 direction = (mousePosition - playerPosition).normalized;
 
         Gizmos.DrawLine(playerPosition, playerPosition + direction * 100);
